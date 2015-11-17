117e1c2c23352c63b64143ba1a75ae6b24f8e173
bq-wang <redalert2c@live.com>
2015-11-18 02:21:39 +0800

    php exception - define custom Exception class; try-throw-catch exceptions. finally are supported php 5.5+

---
+++ 07/README.php
@@ -0,0 +1,13 @@
+<?php
+
+// ----------------------------------------------
+//  Exception
+// ----------------------------------------------
+// 
+// There is n't much to mention on the php exception
+// only after PHP 5.5 or later does it has FINALLY clause support
+// it does not have the ELSE statement as python does.
+// 
+
+
+?>
